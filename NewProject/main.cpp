401f75dc64bde5100255c7d9a30b04651b89a271
GLB12 <1310772052@qq.com>
2019-01-22 23:00:15 +0800

    first commit

---
+++ NewProject/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+int main()
+{
+	int i = 1;
+	while (i)
+	{
+        system("cd C:\\Program Files(x86)\\Tencent\\QQ\\Bin");
+		system("QQ");
+		//int select=MessageBox(NULL,"选择一个取消","打开计算器",2);
+
+		/*if (select==4)
+		{
+			i++;
+		}*/
+	}
+
+	
+	//system("pause");
+	return 0;
+}
